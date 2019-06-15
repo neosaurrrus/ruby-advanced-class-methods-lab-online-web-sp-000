d5b8e5d370965dafb2c47c98c655f87785a77f81
Lukie Kang <lukiek+f@gmail.com>
2019-06-15 19:52:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
 
 
   def self.find_or_create_by_name(name)
-    check = self.find_by_name(name)
+    check = Song.find_by_name(name)
     puts "name #{name} check #{check}"
     if check
       return check
