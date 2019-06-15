746423b8bca066ddb8b81f796d9a3c4ac7b5ca57
Lukie Kang <lukiek+f@gmail.com>
2019-06-15 19:50:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,11 +40,10 @@ class Song
     puts name
     check = self.find_by_name(name)
 
-    if check != false
-      check
+    if check 
+      return check
     elsif check == false
       self.create_by_name(name)
-      puts name
     end
   end # method
 
